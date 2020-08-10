f96e851ae4fb0a929d4c889c737cee2f9345663c
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-10 17:46:39 +0530

    Em  Login Reg

--- app/src/main/java/com/WizGuys/eStudent/Dashboard.java
+++ app/src/main/java/com/WizGuys/eStudent/Dashboard.java
@@ -6,26 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity implements View.OnClickListener {
 
     Button bLogin;
+    TextView tvRegisterLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        bLogin = (Button) findViewById(R.id.bLoginPage);
+        bLogin = (Button) findViewById(R.id.bLogin);
         bLogin.setOnClickListener(this);
+        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+        tvRegisterLink.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
         switch (view.getId()) {
-            case R.id.bLoginPage:
-                startActivity(new Intent(this, Login.class));
+            case R.id.bLogin:
+
                 break;
+                case  R.id.tvRegisterLink:
+                startActivity(new Intent(this,Register.class));
+                break;
+
+
         }
     }
 }
