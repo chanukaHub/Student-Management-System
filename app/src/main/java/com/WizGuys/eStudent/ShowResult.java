6a08ad4daf187ed14b976d6622ebab2137540c99
chanukaHub <chanukadil1999@gmail.com>
2020-08-13 15:49:44 +0530

    AddShowResultPage

---
+++ app/src/main/java/com/WizGuys/eStudent/ShowResult.java
@@ -0,0 +1,29 @@
+package com.WizGuys.eStudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class ShowResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_result);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        // Remove default title text
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // Get access to the custom title view
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+
+
+    }
+}
