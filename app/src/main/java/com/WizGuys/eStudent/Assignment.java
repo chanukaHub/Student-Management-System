a3eb8c5d14a3499754e81674c5cd99d72146c5c8
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-14 21:41:29 +0530

    Complete Dashboard

---
+++ app/src/main/java/com/WizGuys/eStudent/Assignment.java
@@ -0,0 +1,13 @@
+package com.WizGuys.eStudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Assignment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment);
+    }
+}
