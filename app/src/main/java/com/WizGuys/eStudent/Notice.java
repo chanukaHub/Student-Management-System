36b3f30e803becd245734ed0bd553b60901829f0
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-15 00:24:21 +0530

    Add Notice

--- app/src/main/java/com/WizGuys/eStudent/Notice.java
+++ app/src/main/java/com/WizGuys/eStudent/Notice.java
@@ -1,46 +1,33 @@
 package com.WizGuys.eStudent;
 
-import android.annotation.SuppressLint;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+public class Notice extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     TextView textView;
-    ImageButton imageButton;
     Menu menu;
-
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-//        //Hide time battery icons etc...
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        setContentView(R.layout.activity_assignment);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        imageButton = findViewById(R.id.imageView5);
 
         toolbar = findViewById(R.id.toolbar);
 
@@ -51,16 +38,7 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_message);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Dashboard.this, Notice.class);
-                startActivity(intent);
-            }
-        });
     }
-
 
     @Override
     public void onBackPressed() {
@@ -79,26 +57,27 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
             case R.id.nav_message:
                 break;
             case R.id.nav_message1:
-                Intent intent = new Intent(Dashboard.this, MainActivity2.class);
+                Intent intent = new Intent(Notice.this, MainActivity2.class);
                 startActivity(intent);
                 break;
             case R.id.nav_message3:
-                Intent intent1 = new Intent(Dashboard.this, MainActivity4.class);
+                Intent intent1 = new Intent(Notice.this, MainActivity4.class);
                 startActivity(intent1);
 //                menu.findItem(R.id.nav_message3).setVisible(true);
 //                menu.findItem(R.id.nav_share211).setVisible(true);
 //                menu.findItem(R.id.nav_share2111).setVisible(false);
                 break;
 
-            case R.id.nav_share1:
-                Intent intent2 = new Intent(Dashboard.this, Notice.class);
-                startActivity(intent2);
-                break;
             case R.id.nav_share:
                 Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
                 break;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
