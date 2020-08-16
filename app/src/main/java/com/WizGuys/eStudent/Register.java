7c071c82b56e2f1743282583c6e982725e3d2b1b
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-16 21:04:20 +0530

    error

--- app/src/main/java/com/WizGuys/eStudent/Register.java
+++ app/src/main/java/com/WizGuys/eStudent/Register.java
@@ -7,37 +7,33 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Register extends AppCompatActivity implements View.OnClickListener {
+import com.firebase.client.Firebase;
 
+public class Register extends AppCompatActivity {
     Button bRegister;
     EditText etName, etAge, etUserName, etPassword;
+    private Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        etName = (EditText) findViewById(R.id.etName);
-        etAge = (EditText) findViewById(R.id.etAge);
-        etUserName = (EditText) findViewById(R.id.etUserName);
-        etPassword = (EditText) findViewById(R.id.etPassword);
-        bRegister = (Button) findViewById(R.id.bRegister);
 
-        bRegister.setOnClickListener(this);
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://student-management-syste-970d0.firebaseio.com/");
+       bRegister = findViewById(R.id.addBtn);
+
+       bRegister.setOnClickListener(new View.OnClickListener(){
+
+           @Override
+           public void onClick(View view) {
+               Firebase mRefChild = mRef.child("Name");
+               mRefChild.setValue("Rivindu");
+           }
+       });
     }
 
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.bRegister:
-                String name = etName.getText().toString();
-                String userName = etUserName.getText().toString();
-                String password = etPassword.getText().toString();
-                int age = Integer.parseInt(etAge.getText().toString());
 
-                break;
-
-        }
-    }
 
 }
