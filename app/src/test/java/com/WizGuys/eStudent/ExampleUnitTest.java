b0d49f9c90dc0bc3dbfbd1bc584d38190e8cca49
chanukaHub <chanukadil1999@gmail.com>
2020-10-04 15:55:17 +0530

    add AlertBox

--- app/src/test/java/com/WizGuys/eStudent/ExampleUnitTest.java
+++ app/src/test/java/com/WizGuys/eStudent/ExampleUnitTest.java
@@ -19,7 +19,6 @@ public class ExampleUnitTest {
     public void setUp(){
         studentDetails = new StudentDetails();
     }
-
     @Test
     public void studentTotalMarks_isCorrect(){
         float result= studentDetails.getTotalMarks(90,50,70);
@@ -29,6 +28,11 @@ public class ExampleUnitTest {
     public void studentAvgMarks_isCorrect(){
         float result= studentDetails.getAvgMarks(65,63,60);
         assertEquals(62.666668,result,0.001);
+    }
+    @Test
+    public void studentAssignmentAvgMarks_isCorrect(){
+        float result= studentDetails.getAvgCAMarks(80,90);
+        assertEquals(85.0,result,0.001);
     }
 
 
