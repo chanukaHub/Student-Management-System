0f5b62dd8fa374de6d54caa2ddf10f7bad6fd799
chanukaHub <chanukadil1999@gmail.com>
2020-10-17 00:31:40 +0530

    designDashBoard

--- app/src/main/java/com/WizGuys/eStudent/Dashboard.java
+++ app/src/main/java/com/WizGuys/eStudent/Dashboard.java
@@ -54,7 +54,7 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
 
     RelativeLayout todoButton,teachersDashboard;
     //subjectButton
-    RelativeLayout subjectButton,studentButton,resultButton;
+    RelativeLayout subjectButton,studentButton,resultButton,timetableButton,financeButton;
 
 
     @SuppressLint("WrongViewCast")
@@ -67,7 +67,6 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
         //Hooks
         recyclerView = findViewById(R.id.featured_recycler);
         mostViewedRecycler = findViewById(R.id.most_viewed_recycler);
-        categoriesRecycler = findViewById(R.id.categories_recycler);
         menu_Icon = findViewById(R.id.menu_icon);
         contentView = findViewById(R.id.content);
         btnLogOut = findViewById(R.id.imgLogOut);
@@ -86,15 +85,22 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
         //resultButton
         resultButton = findViewById(R.id.resultButton);
 
+        //timetableButton
+        timetableButton = findViewById(R.id.timetableBtn);
+
         //subjectButton
         subjectButton = findViewById(R.id.subjectManager);
+
+        //financeButton
+        financeButton = findViewById(R.id.financeBtn);
+
+
 
 
         navigationDrawer();
 
         featuredRecycler();
         mostViewedRecycler();
-        categoriesRecycler();
 
 
         btnLogOut.setOnClickListener(new View.OnClickListener() {
@@ -149,6 +155,23 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
                 startActivity(intent);
             }
         });
+        timetableButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, TimeTablesDashboard.class);
+                startActivity(intent);
+            }
+        });
+        financeButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, FinanceDashboard.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void navigationDrawer() {
@@ -190,39 +213,16 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
             }
         });
     }
-    private void categoriesRecycler() {
 
-        //All Gradients
-        gradient2 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffd4cbe5, 0xffd4cbe5});
-        gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xff7adccf, 0xff7adccf});
-        gradient3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xfff7c59f, 0xFFf7c59f});
-        gradient4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffb8d7f5, 0xffb8d7f5});
-
-
-        ArrayList<CategoriesHelperClass> categoriesHelperClasses = new ArrayList<>();
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1, R.drawable.capture1, "Education"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient2, R.drawable.capture4, "HOSPITAL"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient3, R.drawable.capture4, "Restaurant"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient4, R.drawable.capture4, "Shopping"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1, R.drawable.capture4, "Transport"));
-
-
-        categoriesRecycler.setHasFixedSize(true);
-        adapter = new CategoriesAdapter(categoriesHelperClasses);
-        categoriesRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        categoriesRecycler.setAdapter(adapter);
-
-    }
 
     private void mostViewedRecycler() {
         mostViewedRecycler.setHasFixedSize(true);
         mostViewedRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<MostViewedHelperClass> mostViewedLocations = new ArrayList<>();
-        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.capture4,"333333333333", "McDonald's"));
-        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.capture4, "33333333332","Edenrobe"));
-        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.capture4, "J.","323"));
-        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.capture4, "Walmart","43322"));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.bell1,"Final Exam 2020", "Final examination will be started on 7th November"));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.bell1, "Study Leaves","Study leaves can be taken after 26th October"));
+
 
         adapter = new MostViewedAdpater(mostViewedLocations);
         mostViewedRecycler.setAdapter(adapter);
@@ -234,7 +234,7 @@ public class Dashboard extends AppCompatActivity implements NavigationView.OnNav
         ArrayList<FeaturedHelper> featuredHelperArrayList = new ArrayList<>();
         featuredHelperArrayList.add(new FeaturedHelper(R.drawable.q1, "Mansa", "CEO at e-Students System"));
         featuredHelperArrayList.add(new FeaturedHelper(R.drawable.q2, "Alisa", "Co-founder"));
-        featuredHelperArrayList.add(new FeaturedHelper(R.drawable.capture4, "545345", "666666666666666666666666666666666666"));
+        featuredHelperArrayList.add(new FeaturedHelper(R.drawable.q3, "Sachini", "Senior Lecturer"));
         adapter = new FeaturedAdapter(featuredHelperArrayList);
         recyclerView.setAdapter(adapter);
 
--- app/src/main/java/com/WizGuys/eStudent/helperClass/MostViewedAdpater.java
+++ app/src/main/java/com/WizGuys/eStudent/helperClass/MostViewedAdpater.java
@@ -1,28 +1,20 @@
-
 package com.WizGuys.eStudent.helperClass;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import com.WizGuys.eStudent.R;
 import com.WizGuys.eStudent.helperClass.helperResources.MostViewedHelperClass;
-
 import java.util.ArrayList;
-
 public class MostViewedAdpater extends RecyclerView.Adapter<MostViewedAdpater.MostViewedViewHolder> {
-
     ArrayList<MostViewedHelperClass> mostViewedLocations;
-
     public MostViewedAdpater(ArrayList<MostViewedHelperClass> mostViewedLocations) {
         this.mostViewedLocations = mostViewedLocations;
     }
-
     @NonNull
     @Override
     public MostViewedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -30,30 +22,26 @@ public class MostViewedAdpater extends RecyclerView.Adapter<MostViewedAdpater.Mo
         MostViewedViewHolder mostViewedViewHolder = new MostViewedViewHolder(view);
         return mostViewedViewHolder;
     }
-
     @Override
     public void onBindViewHolder(@NonNull MostViewedViewHolder holder, int position) {
         MostViewedHelperClass helperClass = mostViewedLocations.get(position);
-
         holder.imageView.setImageResource(helperClass.getImage());
         holder.textView.setText(helperClass.getTitle());
+        holder.mv_desc.setText(helperClass.getDescription());
     }
-
     @Override
     public int getItemCount() {
         return mostViewedLocations.size();
     }
-
     public static class MostViewedViewHolder extends RecyclerView.ViewHolder {
-
         ImageView imageView;
         TextView textView;
-
+        TextView mv_desc;
         public MostViewedViewHolder(@NonNull View itemView) {
             super(itemView);
-
             imageView = itemView.findViewById(R.id.mv_image);
             textView = itemView.findViewById(R.id.mv_title);
+            mv_desc = itemView.findViewById(R.id.mv_desc);
         }
     }
 }
